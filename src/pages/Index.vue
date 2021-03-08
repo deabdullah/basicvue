8231bd0c0ec49888357e9cb59b226bf9a00fc61f
deabdullah <deabdullahmanzoor@gmail.com>
2021-03-08 12:33:00 +0500

    First Commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page class="flex flex-center">
+        <h1>Qpage</h1>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+};
+</script>
